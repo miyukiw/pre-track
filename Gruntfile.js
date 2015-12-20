7d954660a7895adfa40369960cc5c5c089e6d805
MIYUKI WATANABE <miyuki.wtnb@gmail.com>
2015-12-20 23:09:52 +0900

    resolve file path

--- Gruntfile.js
+++ Gruntfile.js
@@ -366,6 +366,7 @@ module.exports = function (grunt) {
           expand: true,
           dest: '<%= yeoman.dist %>',
           src: [
+            'sample.json',
             'package.json',
             'server/**/*'
           ]
--- server/api/track.controller.js
+++ server/api/track.controller.js
@@ -3,6 +3,7 @@
 var _ = require('lodash');
 var http = require('http');
 var fs = require('fs');
+var config = require('../config/environment');
 
 exports.createTrack = function(req, res) {
   var post_data = req.body;
@@ -10,11 +11,14 @@ exports.createTrack = function(req, res) {
   res.json(post_data);
 };
 
+var _FILE = (config.root + '/sample.json');
+console.log(_FILE)
+
 // get transaction records
 exports.getTrack = function(req, res) {
   var trackId = req.param('id');
   //console.log(trackId);
-  fs.readFile('sample.json', function (err, data) {
+  fs.readFile(_FILE, function (err, data) {
     if (err) throw err;
     res.status(200).send(JSON.parse(data));
   });
@@ -25,9 +29,10 @@ exports.updateTrack = function(req, res) {
   var data = req.body;
   console.log(data);
 
-  fs.writeFile('sample.json', JSON.stringify(data), function (err) {
+  fs.writeFile(_FILE, JSON.stringify(data), function (err) {
     if (err) throw err;
     console.log('updated!');
     res.status(200).send();
   });
 };
+
