0a9f76c0b28318a2e8752d75dbefdd2b15bcccc1
MIYUKI WATANABE <miyuki.wtnb@gmail.com>
2015-12-22 17:04:44 +0900

    modify scroll position

--- client/app/main.controller.js
+++ client/app/main.controller.js
@@ -5,4 +5,8 @@ angular.module('preTrackApp')
     $scope.goto = function(path) {
       $location.path(path);
     }
+
+    $scope.$on('$locationChangeStart', function(event) {
+      window.scroll(0, 0);
+    });
   }]);
